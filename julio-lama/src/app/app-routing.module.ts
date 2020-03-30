916f747691a0ede2f11b7989c3f5f15d5fc3f16e
Julio <lamajulio17@gmail.com>
2020-03-29 21:26:07 -0500

    Creating components for website

--- julio-lama/src/app/app-routing.module.ts
+++ julio-lama/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MenuComponent} from './menu/menu.component';
+import {ResumeComponent} from './resume/resume.component';
+import {AboutMeComponent} from './about-me/about-me.component';
+import {ContactMeComponent} from './contact-me/contact-me.component';
+import {ProjectsComponent} from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MenuComponent
+  },
+  {
+    path: 'resume', component: ResumeComponent
+  },
+  {
+    path: 'about-me', component: AboutMeComponent
+  },
+  {
+    path: 'contact-me', component: ContactMeComponent
+  },
+  {
+    path: 'projects', component: ProjectsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
